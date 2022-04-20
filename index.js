ececbb507c547cf76e442603018b5aaec9c1d313
rihilke <perevetish@yandex.ru>
2022-04-20 23:57:05 +0300

    move functions

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
+//0 стена
+//1 проход
+
 let weidth = 1024;
 let height = 640;
 let grid = 50;
 
 let row = Math.ceil(height / grid) ;
 let col = Math.ceil(weidth / grid) ;
+//карта проходов
 let playfield = new Array(row);
     for(var i = 0; i < row; i++){
         playfield[i] = new Array(col);
         for(let j = 0; j < col; j++){
             playfield[i][j] = 0;
         }
+    } 
+//карта объектов
+let entityField = new Array(row);
+for(var i = 0; i < row; i++){
+    playfield[i] = new Array(col);
+    for(let j = 0; j < col; j++){
+        playfield[i][j] = 0;
     }
-//
+}    
 function getPassages(){
     // проложить вертикальные и горизонтальные проходы
     let rowRandom = new Array(getRandomInt(3, 5));
@@ -51,7 +62,6 @@ function getRooms(){
         roomsRandom[i][3] = roomsRandom[i][2] + getRandomInt(3, 8);
             if(roomsRandom[i][3] > col) 
             roomsRandom[i][3] = col;
-        //console.log(roomsRandom[i]);
     }
 
     //нарисовать комнаты
@@ -65,9 +75,7 @@ function getRooms(){
                     i <= roomsRandom[g][3]
                 ){
                     playfield[i][j] = 1;
-                    //console.log(j + ' ' + i);
                 }
-                
             }
         }
     }
@@ -78,55 +86,229 @@ function drawMap(){
     for(var i = 0; i < row; i++){ 
         for(var j = 0; j < col; j++){       
             if(playfield[i][j] === 1){
+                //1 проход
                 let parent = document.querySelector('.field');
                 let p = document.createElement('div');
                 p.className = 'tile';
-                p.style.left = (grid*j)+'px';
                 p.style.top = (grid*i)+'px';
+                p.style.left = (grid*j)+'px';
                 parent.append(p);
             }
+            //0 стена
             if(playfield[i][j] === 0){
                 let parent = document.querySelector('.field');
                 let p = document.createElement('div');
                 p.className = 'tileW';
-                p.style.left = (grid*j)+'px';
                 p.style.top = (grid*i)+'px';
+                p.style.left = (grid*j)+'px';
                 parent.append(p);
             }
         }
     }
 }
 
+class Entity {
+    constructor(id){
+        this.coordinate = getRandomCoordinate();
+        this.id = id;
+    }
 
+    createInPlace(){
+        let parent = document.querySelector('.field');
+        let p = document.createElement('div');
+        p.className = this.className;
+        p.id = this.id;
+        p.style.top = (grid*this.coordinate[0])+'px';
+        p.style.left = (grid*this.coordinate[1])+'px';
+        playfield[this.coordinate[0]][this.coordinate[1]] = this.mark;
+        parent.append(p);
+    }
+    
+    moveUP(){
+        if( playfield[this.coordinate[0]-1][this.coordinate[1]] > 0  &&
+            playfield[this.coordinate[0]-1][this.coordinate[1]] < row &&
+            playfield[this.coordinate[0]-1][this.coordinate[1]] === 1
+            )
+            {
+                //1 проход
+                playfield[this.coordinate[0]][this.coordinate[1]] = 1;
+                this.coordinate[0] = this.coordinate[0]-1;
+                let el = document.getElementById(this.id);
+                el.style.top = (grid*this.coordinate[0])+'px';
+                el.style.left = (grid*this.coordinate[1])+'px';
+                playfield[this.coordinate[0]][this.coordinate[1]] = this.mark;
+            } 
+    }
 
-function Sword(){
-    draw('tileSW');
+    moveDOWN(){
+        if( playfield[this.coordinate[0]+1][this.coordinate[1]] === 1 &&
+            playfield[this.coordinate[0]+1][this.coordinate[1]] > 0  &&
+            playfield[this.coordinate[0]+1][this.coordinate[1]] < row
+            )
+            {
+                //1 проход
+                playfield[this.coordinate[0]][this.coordinate[1]] = 1;
+                this.coordinate[0] = this.coordinate[0]+1;
+                let el = document.getElementById(this.id);
+                el.style.top = (grid*this.coordinate[0])+'px';
+                el.style.left = (grid*this.coordinate[1])+'px';
+                playfield[this.coordinate[0]][this.coordinate[1]] = this.mark;
+            }
+    }
+    moveRIGHT(){
+        if( playfield[this.coordinate[0]][this.coordinate[1]+1] === 1 &&
+            playfield[this.coordinate[0]][this.coordinate[1]+1] > 0  &&
+            playfield[this.coordinate[0]][this.coordinate[1]+1] < row
+            )
+            {
+                //1 проход
+                playfield[this.coordinate[0]][this.coordinate[1]] = 1;
+                this.coordinate[1] = this.coordinate[1]+1;
+                let el = document.getElementById(this.id);
+                el.style.top = (grid*this.coordinate[0])+'px';
+                el.style.left = (grid*this.coordinate[1])+'px';
+                playfield[this.coordinate[0]][this.coordinate[1]] = this.mark;
+            }
+    }
+
+    moveLEFT(){
+        if( playfield[this.coordinate[0]][this.coordinate[1]-1] === 1 &&
+            playfield[this.coordinate[0]][this.coordinate[1]-1] > 0  &&
+            playfield[this.coordinate[0]][this.coordinate[1]-1] < row
+            )
+            {
+                //1 проход
+                playfield[this.coordinate[0]][this.coordinate[1]] = 1;
+                this.coordinate[1] = this.coordinate[1]-1;
+                let el = document.getElementById(this.id);
+                el.style.top = (grid*this.coordinate[0])+'px';
+                el.style.left = (grid*this.coordinate[1])+'px';
+                playfield[this.coordinate[0]][this.coordinate[1]] = this.mark;
+            }
+    }
+    /*
+    arrRandomMove = [this.moveUP, this.moveDOWN, this.moveLEFT, this.moveRIGHT];
+    moveRandom() {
+       return arrRandomMove[getRandomInt(0, this.arrRandomMove.length)]();
+    }
+    */
+    
+    delete(){
+        let el = document.getElementById(this.id);
+        //1 проход
+        playfield[this.coordinate[0]][this.coordinate[1]] = 1;
+        el.remove();
+    }
+
+}
+//конец Сущности
+class Sword extends Entity{
+    className ='tileSW';
+    mark = 'Sword';
 }
 
-function HealingPotion(){
-    draw('tileHP');
+class HealingPotion extends Entity{
+    className = 'tileHP';
+    mark = 'HP';
 }
 
-function Enemy(){
-    draw('tileE');
+class Person extends Entity{
+    maxHealth = 2;
+    health = 2;
+    hit = 1;
+
+    showFullHealth(){
+        let parent = document.querySelector('.'+this.className);
+        let p = document.createElement('div');
+        p.className = 'health';
+        p.style.width = grid;
+        p.style.position = 'relative';
+//        p.style.top = (grid*this.coordinate[0])+'px';
+//        p.style.left = (grid*this.coordinate[1])+'px';
+        parent.append(p);
+    }
 }
 
-function Player(){
-    draw('tileP');
+class Enemy extends Person{
+    className ='tileE';
+    mark = 'Enemy';
+    health = 1;
+}
+
+class Player extends Person{
+    className ='tileP';
+    mark = 'Player';
+
 }
 
 window.onload = function() {
     getPassages();
     getRooms();
     drawMap();
-    Sword();
-    Sword();
-    for(let i = 0; i < 10; i++){
-        HealingPotion();
-        Enemy();
-    }
-    Player();
+    let sword = new Sword('s');
+    sword.createInPlace();
 
+    //TODO factory
+    
+    let HP   = new HealingPotion('hp');
+    HP.createInPlace();
+    let enemy = new Enemy('e');
+    enemy.createInPlace();
+    enemy.showFullHealth();
+    
+    let player = new Player('player');
+    player.createInPlace();
+    player.showFullHealth();
+
+    document.addEventListener('keydown', function(e) {
+        //Space
+        if (e.which ===  32){
+        //    sword.delete();
+        }
+        //Arrow Up, W	
+        if( e.which === 38 || e.which === 87 ){
+            player.moveUP();
+        }
+
+        //Arrow Down, S	
+        if( e.which === 40 || e.which === 83 ){
+            player.moveDOWN();
+        }
+        //Arrow Right, D	
+        if( e.which === 39 || e.which === 68 ){
+            player.moveRIGHT();
+        }
+        //Arrow Left, A	
+        if( e.which === 37 || e.which === 65 ){
+            player.moveLEFT();
+        }
+
+        //random
+        if(
+            e.which === 38 || e.which === 87 ||
+            e.which === 40 || e.which === 83 ||
+            e.which === 39 || e.which === 68 ||
+            e.which === 37 || e.which === 65
+        ){
+            let i = getRandomInt(0, 3)
+            switch(i) {
+                case 0:  
+                    enemy.moveDOWN();
+                    break
+                case 1:  
+                    enemy.moveLEFT();
+                    break
+                case 2:
+                    enemy.moveRIGHT();
+                    break
+                case 3:
+                    enemy.moveUP();
+                    break
+                default:
+                    break
+            }
+        }
+    });
 };
 
 /** утилс**/
@@ -139,22 +321,38 @@ function getRandomInt(min, max) {
 function getRandomCoordinate(){
     let x = getRandomInt(0, row-1);
     let y = getRandomInt(0, col-1);
-    do{
+    do {
         x = getRandomInt(0, row-1);
         y = getRandomInt(0, col-1);
-    } while (playfield[x][y] === 0);
+    } while (playfield[x][y] != 1);
 
     let coordinate = [x,y];
-    console.log(coordinate);
     return coordinate;
 }
 
-function draw(className){
+function createInRandomPlace(className){
     let coordinate = getRandomCoordinate();
     let parent = document.querySelector('.field');
     let p = document.createElement('div');
     p.className = className;
+    p.style.top  = (grid*coordinate[0])+'px';
     p.style.left = (grid*coordinate[1])+'px';
-    p.style.top = (grid*coordinate[0])+'px';
     parent.append(p);
 }
+
+/*
+function draw(coordinate){
+    this.style.top = (grid*coordinate[0])+'px';
+    this.style.left = (grid*coordinate[1])+'px';
+}
+*/
+/*
+function createInPlace(className, coordinate){
+    let parent = document.querySelector('.field');
+    let p = document.createElement('div');
+    p.className = className;
+    p.style.top = (grid*coordinate[0])+'px';
+    p.style.left = (grid*coordinate[1])+'px';
+    parent.append(p);
+}
+*/
